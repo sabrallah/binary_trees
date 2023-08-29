7cc77c20941dbed1d3d4bf01039ac77c8fd4f2b2
sabrallah <sabrallah1991@gmail.com>
2023-08-29 11:49:11 -0700

    task20

---
+++ 101-binary_tree_levelorder.c
@@ -0,0 +1,56 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_levelorder - aaaaaaaaaaaaaaaaaaaaaaa
+ * @tree: aaaaaaaaaaaaaaaaaaaaa
+ * @func: aaaaaaaaaaaaaaaaaaaaaaaaaaaa
+ */
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+{
+	size_t mlevel, smaxlevel;
+
+	if (!tree || !func)
+		return;
+
+	smaxlevel = binary_tree_height(tree) + 1;
+
+	for (mlevel = 1; mlevel <= smaxlevel; mlevel++)
+		sbtlo_helper(tree, func, mlevel);
+}
+
+/**
+ * sbtlo_helper - aaaaaaaaaaaaaaaaaaaaaaa
+ * @tree: fffffffffffffffffffffff
+ * @func: hhhhhhhhhhhhhhhhhhhhhhh
+ * @mlevel: jjjjjjjjjjjjjjjjjjjjjjjjjjj
+ */
+void sbtlo_helper(const binary_tree_t *tree, void (*func)(int), size_t mlevel)
+{
+	if (mlevel == 1)
+		func(tree->n);
+	else
+	{
+		sbtlo_helper(tree->left, func, mlevel - 1);
+		sbtlo_helper(tree->right, func, mlevel - 1);
+	}
+}
+
+/**
+ * binary_tree_height - fffffffffffffff
+ * @tree: gggggggggggggggggggggg
+ *
+ * Return: ggggggggggggggge
+ *         0 ggggggggggggggggggg
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t mheight_l = 0;
+	size_t mheight_r = 0;
+
+	if (!tree)
+		return (0);
+
+	mheight_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	mheight_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (mheight_l > mheight_r ? mheight_l : mheight_r);
+}
