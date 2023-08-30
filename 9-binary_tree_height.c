5833329e59490447ce1b68cd67f33ece3c7bc698
sabrallah <sabrallah1991@gmail.com>
2023-08-30 13:48:02 -0700

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,23 @@
 #include "binary_trees.h"
 
 
+
 /**
- * binary_tree_height - hauteur d'un arbre binaire
- * @tree: eur d'un arbre binaire
+ * binary_tree_height - hauteur tttt d'un arbre binaire
+ * @tree: eur d'un arbretttt binaire
  *
- * Return: hauteur d'un arbre binaire
- *         mesure d'un arbre binaire
+ * Return: hauteur tttd'un arbre binaire
+ *         mesure d'un gggarbre binaire
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_lef = 0;
-	size_t height_rig = 0;
+size_t height_lef = 0;
+size_t height_rig = 0;
 
-	if (!tree)
-		return (0);
+if (!tree)
+return (0);
 
-	height_lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_lef > height_rig ? height_lef : height_rig);
+height_lef = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+height_rig = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+return (height_lef > height_rig ? height_lef : height_rig);
 }
