c1ec2580f6f03de1996443e8b5650cdf7ec115d4
sabrallah <sabrallah1991@gmail.com>
2023-08-30 13:41:41 -0700

    task8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -3,15 +3,15 @@
 
 /**
  * binary_tree_postorder - traverse binaire en utilisant la traverse post-ordre
- * @tree: traverse un arbre binaire en utilisant la traverse post-ordre
- * @func: traverse un arbre binaire en utilisant la traverse post-ordre
+ * @tree: traverse un arbre binaire en uutilisant la ttraverse post-ordre
+ * @func: traverse un arbre binaire en uutilisant la ttraverse post-ordre
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
+if (!tree || !func)
+	return;
 
-	binary_tree_postorder(tree->left, func);
-	binary_tree_postorder(tree->right, func);
-	func(tree->n);
+binary_tree_postorder(tree->left, func);
+binary_tree_postorder(tree->right, func);
+func(tree->n);
 }
