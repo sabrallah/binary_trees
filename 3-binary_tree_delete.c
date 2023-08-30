2a2bd37994db3a53b78029d1463a5ef8c5236248
sabrallah <sabrallah1991@gmail.com>
2023-08-30 13:11:15 -0700

    task3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,22 @@
 #include "binary_trees.h"
 
+void binary_tree_delete(binary_tree_t *tree) {
 
-/**
- * binary_tree_delete - supprime un arbre avec binaire entier
- * @tree: supprime un arbre binaire avec entier
- */
-void binary_tree_delete(binary_tree_t *tree)
+if(tree == NULL) 
 {
-	if (!tree)
-		return;
-	binary_tree_delete(tree->right);
+	return;
+}
+
+if(tree->left != NULL) 
+{
 	binary_tree_delete(tree->left);
+}
+
+if(tree->right != NULL) 
+{
+	binary_tree_delete(tree->right);
+}
+
 	free(tree);
+
 }
